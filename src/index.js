c898ada45c0271a8a6e2384742888b3e843b456d
Oardieldei <alik.want.you@gmail.com>
2022-07-31 14:46:13 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 
-// You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+	let arr = []
+
+	if (!matrix) return arr
+
+	for (let i = 0; i < matrix.length; i++) {
+		let newItem = []
+
+		for (let j = 0; j < matrix[i].length; j++) {
+			if (i % 2 == 0) {
+				newItem.push(matrix[i][j])
+			} else {
+				newItem.unshift(matrix[i][j])
+			}
+		}
+
+		arr = arr.concat(newItem)
+	}
+
+	return arr
 }
